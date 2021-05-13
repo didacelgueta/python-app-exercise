d8928b21302032601f37f056ba1d9a6d2e5ecf59
didacelgueta <didac.elgueta@gmail.com>
2021-05-13 18:15:00 +0200

    Connection to API and save JSON in differents CSV

--- src/Application/App.py
+++ src/Application/App.py
@@ -3,7 +3,7 @@ from src.Services.ApiService import ApiService
 
 class App:
     def __init__(self):
-        self._api_service = ApiService()
+        self._api_service = ApiService('https://jsonplaceholder.typicode.com/todos/')
 
     def api_service(self) -> ApiService:
         return self._api_service
--- src/Services/ApiService.py
+++ src/Services/ApiService.py
@@ -1,11 +1,23 @@
 from sys import stderr
+import requests
+import json
+import pandas as pd
+import datetime as dt
 
+today = dt.datetime.now().strftime("%Y_%m_%d")
 
 class ApiService:
-    def __init__(self):
-        pass
+    def __init__(self, url):
+        self.url = url
+
+    def save_data(self, json):
+        df_input = pd.DataFrame.from_dict(json, orient='index')
+        df_input.to_csv("./storage/" + today + "_" + str(json['id']) + ".csv", sep=";", header=None)
 
     def run(self):
-        print('Running ApiService', file=stderr)
+        with requests.session() as session:
+            response = session.get(self.url)
+            json_response = json.loads(response.text)
 
-        # TODO: follow README.md instructions
+        for todo in json_response:
+            self.save_data(todo)

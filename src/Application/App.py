af4bd251716a8925eb47734d66e5c62df1a6b4c7
didacelgueta <didac.elgueta@gmail.com>
2021-05-14 11:48:16 +0200

    solve conflict

--- src/Application/App.py
+++ src/Application/App.py
@@ -5,11 +5,7 @@ class App:
     URL = 'https://jsonplaceholder.typicode.com/todos/'
 
     def __init__(self):
-<<<<<<< HEAD
         self._api_service_save_csv = ApiServiceSaveCsv(self.URL)
-=======
-        self._api_service = ApiService('https://jsonplaceholder.typicode.com/todos/')
->>>>>>> d8928b21302032601f37f056ba1d9a6d2e5ecf59
 
     def api_service_save_csv(self) -> ApiServiceSaveCsv:
         return self._api_service_save_csv
